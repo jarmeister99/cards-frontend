56008c26f38aa6db366c0a56a5fdf04d65a6673e
Jared Foster <jfoster@rantec.com>
2021-11-11 14:51:27 -0800

    populated with duplicate data

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,77 @@ function App() {
   const cards: ICard[] = [
     {
       title: 'Knowledge Base',
-      teaser: 'Contains code snippets, technical articles, and blog posts related to various technologies' 
+      teaser: 'Contains code snippets, technical articles, and blog posts related to various technologies',
+      content: 'Dummy content'
+
     },
     {
       title: 'Magic of CSS',
       teaser: 'A visual tutorial of many CSS concepts',
       link: 'https://adamschwartz.co/magic-of-css/',
-      content: 'Covers a lot of useful stuff'
+      content: 'Dummy content'
     },
     {
       title: 'Styled-Components Getting Started Guide',
-      teaser: 'A first-party beginner\'s tutorial to the styled-components library' 
+      teaser: 'A first-party beginner\'s tutorial to the styled-components library',
+      content: 'Dummy content'
+
     },
     {
       title: 'Basic Concepts of Flexbox',
-      teaser: 'A tutorial from mozilla that explains how CSS flexbox works'
+      teaser: 'A tutorial from mozilla that explains how CSS flexbox works',
+      content: 'Dummy content'
     },
-  ] 
+    {
+      title: 'Knowledge Base',
+      teaser: 'Contains code snippets, technical articles, and blog posts related to various technologies',
+      content: 'Dummy content'
+
+    },
+    {
+      title: 'Magic of CSS',
+      teaser: 'A visual tutorial of many CSS concepts',
+      link: 'https://adamschwartz.co/magic-of-css/',
+      content: 'Dummy content'
+    },
+    {
+      title: 'Styled-Components Getting Started Guide',
+      teaser: 'A first-party beginner\'s tutorial to the styled-components library',
+      content: 'Dummy content'
+
+    },
+    {
+      title: 'Basic Concepts of Flexbox',
+      teaser: 'A tutorial from mozilla that explains how CSS flexbox works',
+      content: 'Dummy content'
+    },
+    {
+      title: 'Knowledge Base',
+      teaser: 'Contains code snippets, technical articles, and blog posts related to various technologies',
+      content: 'Dummy content'
+
+    },
+    {
+      title: 'Magic of CSS',
+      teaser: 'A visual tutorial of many CSS concepts',
+      link: 'https://adamschwartz.co/magic-of-css/',
+      content: 'Dummy content'
+    },
+    {
+      title: 'Styled-Components Getting Started Guide',
+      teaser: 'A first-party beginner\'s tutorial to the styled-components library',
+      content: 'Dummy content'
+
+    },
+    {
+      title: 'Basic Concepts of Flexbox',
+      teaser: 'A tutorial from mozilla that explains how CSS flexbox works',
+      content: 'Dummy content'
+    },
+  ]
   return (
     <div className="App">
-      <CardGallery cards={cards}/>
+      <CardGallery cards={cards} />
     </div>
   );
 }
