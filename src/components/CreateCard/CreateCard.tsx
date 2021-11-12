da7cb1be891ea2e1a028ae2f602e9e7dd0593e87
Jared Foster <jfoster@rantec.com>
2021-11-12 13:49:29 -0800

    added cool expanding create button

--- src/components/CreateCard/CreateCard.tsx
+++ src/components/CreateCard/CreateCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { PaleVioletButton } from '../Generic/Buttons/Buttons';
+import '../Generic/Buttons/Buttons.scss'
 
 const CreateCard: React.FC = (props): JSX.Element => {
     return (
         <div>
-
+            <PaleVioletButton primary={true} className="expand">Create</PaleVioletButton>
         </div>
     )
 }
---
+++ src/components/Generic/Buttons/Buttons.tsx
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+
+const PaleVioletButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+  font-size: 1em;
+  :hover {
+    cursor: pointer;
+  }
+  ${(props: { primary: boolean; }) => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+
+export { PaleVioletButton }
