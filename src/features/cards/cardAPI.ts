d22016504d341f57a273b3dbf2b0d794678b4648
jarmeister <jarmeister99@gmail.com>
2021-12-01 21:53:41 -0800

    typo on fetchCards

--- src/features/cards/cardAPI.ts
+++ src/features/cards/cardAPI.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const fetchCards = () => {
-  return axios.get(`${process.env.REACT_APP_API_URI}shares/`)
+  return axios.get(`${process.env.REACT_APP_API_URI}/shares/`)
 }
